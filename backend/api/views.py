c6f4934d3cccd8ee0d130c0175c7dd03a44bb88c
Дмитрий Васин <vasinlo0@gmail.com>
2022-10-30 20:39:26 +0300

    test:1

--- backend/api/views.py
+++ backend/api/views.py
@@ -47,7 +47,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def favorite(self, request, pk=None):
         if request.method == 'GET':
             return self.add_obj(Favorite, request.user, pk)
-        elif request.method == 'DELETE':
+        if request.method == 'DELETE':
             return self.delete_obj(Favorite, request.user, pk)
         return None
 
@@ -56,7 +56,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def shopping_cart(self, request, pk=None):
         if request.method == 'GET':
             return self.add_obj(Cart, request.user, pk)
-        elif request.method == 'DELETE':
+        if request.method == 'DELETE':
             return self.delete_obj(Cart, request.user, pk)
         return None
 
