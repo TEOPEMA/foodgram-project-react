56e23a575343f3c7183c0e8632b8547af1774f97
Дмитрий Васин <vasinlo0@gmail.com>
2023-02-11 01:39:09 +0300

    рep8

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -2,7 +2,7 @@ from rest_framework import permissions
 
 
 class AuthorAdminOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj): 
+    def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
             or (
